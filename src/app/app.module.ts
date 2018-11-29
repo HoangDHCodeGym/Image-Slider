b556e9b7e38fbc444545cca63afbab0dc2a6936c
HoangDHCodeGym <ol.hoangdh.thna@gmail.com>
2018-11-29 09:59:00 +0700

    Update Gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageSliderComponent } from './image-slider/image-slider.component';
+import { ImgSlideComponent } from './image-slider/img-slide/img-slide.component';
+import {ImageGalleryModule} from './image-gallery/image-gallery.module';
+import {GalleryConfig} from './image-gallery/token';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageSliderComponent,
+    ImgSlideComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageGalleryModule
   ],
-  providers: [],
+  providers: [{provide: GalleryConfig, useValue: 2}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
